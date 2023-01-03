e0b67602ac079c19a96a10b9b3ff282a394cdbc0
mikesamotaev <samotaev.mikhail@gmail.com>
2023-01-03 12:31:52 +0300

    initial

---
+++ pages/ninjas/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../../styles/Ninjas.module.css'
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: {
+      ninjas: data,
+    }
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
+  return (
+    <>
+      <Head>
+        <title>Ninjas list | Ninjas</title>
+      </Head>
+      <div>
+        <h1>Ninjas</h1>
+        {ninjas.map((ninja) => (
+          <Link href={`/ninjas/${ninja.id}`} key={ninja.id} className={styles.single}>
+            <h3>{ ninja.name }</h3>
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+}
+ 
+export default Ninjas;
